40a44aeea39b1888d3d7dffe0a0771de71064eb6
Jacob Millner <jacob.millner@outlook.com>
2019-03-04 19:15:56 -0500

    route authentication

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { map } from 'rxjs/operators/map';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -24,6 +25,15 @@ export class AuthService {
       .pipe(map(data => (data)));
   }
 
+  getProfile() {
+    this.loadToken();
+    let headers = new Headers();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:6969/users/profile', { headers: headers })
+      .pipe(map(data => (data)));
+  }
+
   storeUserData(token, user) {
     localStorage.setItem('id_token', token);
     localStorage.setItem('user', JSON.stringify(user));
@@ -35,6 +45,15 @@ export class AuthService {
     this.authToken = null;
     this.user = null;
     localStorage.clear();
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  loggedIn() {
+    return tokenNotExpired('id_token');
   }
 
 }
